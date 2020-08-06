5739efccaf8e752dad9b80f40b245f3efc05395b
Michael Mallowe <mmallowe@gmail.com>
2020-08-05 23:40:43 -0700

    shadowom to handle heavy lifting and encapsulate components; randomuser.me images

--- userCard.js
+++ userCard.js
@@ -1,7 +1,23 @@
+const template = document.createElement('template')
+template.innerHTML = `
+    <style>
+    h3 {color: coral}
+    </style>
+    <div class="user-card">
+        <img />
+        <div>
+            <h3></h3>
+        </div>
+    </div>
+`
 class UserCard extends HTMLElement {
     constructor() {
         super()
-        this.innerHTML = `${this.getAttribute('name')}`
+        this.attachShadow({mode: 'open'})
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name')
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar')
+        // this.innerHTML = ``
     }
 }
 
