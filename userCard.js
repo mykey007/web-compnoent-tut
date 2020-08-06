42d5b143149092312df4b6f32a2a906dd35e6abe
Michael Mallowe <mmallowe@gmail.com>
2020-08-05 23:21:22 -0700

    adds user card and name attribute

---
+++ userCard.js
@@ -0,0 +1,9 @@
+class UserCard extends HTMLElement {
+    constructor() {
+        super()
+        this.innerHTML = `${this.getAttribute('name')}`
+    }
+}
+
+
+window.customElements.define('user-card', UserCard)
